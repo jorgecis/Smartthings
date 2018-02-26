8cc1b9fd65d1c0c4f37804c2b4cde0f9abf249eb
Jorge Cisneros <jorgecis@gmail.com>
2018-02-26 00:50:50 -0600

    Work in progress, OAUTH2 is working and I got the list of controls

--- smartapps/jorgeci/remotsy.src/remotsy.groovy
+++ smartapps/jorgeci/remotsy.src/remotsy.groovy
@@ -18,7 +18,7 @@
 definition(
     name: "Remotsy",
     namespace: "jorgeci",
-    author: "jorgecis@gmail.com.com",
+    author: "jorgecis@gmail.com",
     description: "Manager the controls from Remotsy",
     category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
@@ -28,11 +28,12 @@ definition(
 
 preferences {
     page(name: "Credentials", title: "Remotsy", content: "authPage", install: false)
+	page(name: "listDevices", title: "Remotsy Controls", content: "listDevices", install: false)  
 }
 
 mappings {
-	path("/receivedToken") 	{ action: [ POST: "receivedToken", GET: "receivedToken"] }
-	path("/receiveToken") 	{ action: [ POST: "receiveToken", GET: "receiveToken"] }
+    path("/oauth/initialize") {action: [GET: "oauthInitUrl"]}
+    path("/oauth/callback") {action: [GET: "callback"]}
 }
 
 private getVendorName() 	{ "Remotsy" }
@@ -40,9 +41,8 @@ private getVendorAuthPath()	{ "https://www.remotsy.com:8443/oauth/authorize?" }
 private getVendorTokenPath(){ "https://www.remotsy.com:8443/oauth/token" }
 private getClientId() 		{ "smartthings" } 
 private getClientSecret() 	{ "kjgsdfslskudfgj" }
-private getVendorIcon()		{ "https://s3.amazonaws.com/smartthings-device-icons/custom/super-widgets/beertap@2x.png" }
-private getServerUrl() 		{ return "https://graph-na04-useast2.api.smartthings.com" }
-
+private getVendorIcon()		{ "https://www.remotsy.com/static/img/remotsy_o.png" }
+private apiUrl() 			{ "https://remotsy.com/rest/" }
 
 
 def authPage() {
@@ -52,17 +52,78 @@ def authPage() {
         log.debug "About to create access token."
         createAccessToken()
         description = "Tap to enter Credentials."
-        def redirectUrl = oauthInitUrl()
-        return dynamicPage(name: "Credentials", title: "Authorize Connection", nextPage: null, uninstall: false, install:false) {
+        def redirectUrl = "https://graph.api.smartthings.com/oauth/initialize?appId=${app.id}&access_token=${state.accessToken}&apiServerUrl=${getApiServerUrl()}"
+        return dynamicPage(name: "Credentials", title: "Authorize Connection", nextPage: "listDevices", uninstall: false, install:false) {
                section { href url:redirectUrl, style:"embedded", required:false, title:"Connect to ${getVendorName()}:", description:description }
         }
     } else {
-    	description = "Press 'Done' to proceed" 
- 		
-		return dynamicPage(name: "Credentials", title: "Credentials Accepted!", nextPage: null, uninstall: true, install:true) {
-               section { href url: buildRedirectUrl("receivedToken"), style:"embedded", required:false, title:"${getVendorName()} is now connected to SmartThings!", description:description }
+        return dynamicPage(name: "Credentials", title: "Authorize Connection", nextPage: "listDevices", uninstall: true, install:false) {
+               section { href url:redirectUrl, style:"embedded", required:false, title:"Connect to ${getVendorName()}:", description:description }
+        }    }
+}
+
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	listDevices()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+    initialize()
+	listDevices()
+}
+
+def listDevices()
+{
+	log.debug "In listDevices"
+
+	def devices = getDeviceList()
+	log.debug "Device List = ${devices}"
+    log.debug "Settings List = ${settings}"
+
+	dynamicPage(name: "listDevices", title: "Choose devices", install: true) {
+		section("Devices") {
+			input "devices", "enum", title: "Select Device(s)", required: false, multiple: true, options: devices
+		}
+	}
+}
+
+
+
+def initialize()
+{
+	log.debug "Initialized with settings: ${settings}"
+
+	settings.devices.each {
+		def deviceId = it
+        log.info it
+		state.deviceDataArr.each {
+        
         }
     }
+}
+def getDeviceList()
+{
+	log.debug "In getDeviceList"
+
+	def deviceList = [:]
+	state.deviceDataArr = []
+
+	apiPost("controls/list") { response ->
+        response.data.data.controls.each() {
+				deviceList["${it._id}"] = it.name
+				state.deviceDataArr.push(['name'    : it.name,
+										  'id'      : it._id,
+                                          'iddev'   : it.iddev
+										  
+				])
+			}
+            
+        
+     }
+     return deviceList
+
 }
 
 def oauthInitUrl() {
@@ -73,35 +134,27 @@ def oauthInitUrl() {
         client_id: getClientId(),
         client_secret: getClientSecret(),
         state: state.oauthInitState,
-        redirect_uri: buildRedirectUrl("receiveToken")
+        redirect_uri: "https://graph.api.smartthings.com/oauth/callback"
     ]
-    return getVendorAuthPath() + toQueryString(oauthParams)
+    redirect(location: getVendorAuthPath() + toQueryString(oauthParams))
 }
 
-def buildRedirectUrl(endPoint) {
-	log.debug "In buildRedirectUrl"
-    log.debug getServerUrl()+ "/api/token/${state.accessToken}/smartapps/installations/${app.id}/${endPoint}"
-    return getServerUrl() + "/api/token/${state.accessToken}/smartapps/installations/${app.id}/${endPoint}"
-}
 
-def receiveToken() {
-	log.debug "In receiveToken"
+def callback() {
     def oauthParams = [ client_id: getClientId(),
                         client_secret: getClientSecret(),
     				    grant_type: "authorization_code", 
                         code: params.code,
-                        redirect_uri: buildRedirectUrl("receiveToken")]                        
+                        redirect_uri: "https://graph.api.smartthings.com/oauth/callback"]                        
 	httpPost(getVendorTokenPath(), toQueryString(oauthParams)) { response -> 
     	state.vendorRefreshToken = response.data.refresh_token
         state.vendorAccessToken = response.data.access_token
 	}
      
-    if ( !state.vendorAccessToken ) {  //We didn't get an access token, bail on install
+    if ( !state.vendorAccessToken ) {  
     	return
     }
-    
-    /* OAuth Step 3: Use the access token to call into the vendor API throughout your code using state.vendorAccessToken. */
-       
+          
     def html = """
         <!DOCTYPE html>
         <html>
@@ -149,11 +202,6 @@ def receiveToken() {
                 padding: 0 40px;
                 margin-bottom: 0;
             }
-        /*
-            p:last-child {
-                margin-top: 0px;
-            }
-        */
             span {
                 font-family: 'Swiss 721 W01 Light';
             }
@@ -173,79 +221,86 @@ def receiveToken() {
 	render contentType: 'text/html', data: html
 }
 
-def receivedToken() {
-	log.debug "In receivedToken"
-    
-    def html = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <meta name="viewport" content="100%">
-        <title>Withings Connection</title>
-        <style type="text/css">
-            @font-face {
-                font-family: 'Swiss 721 W01 Thin';
-                src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.eot');
-                src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.eot?#iefix') format('embedded-opentype'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.woff') format('woff'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.ttf') format('truetype'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-thin-webfont.svg#swis721_th_btthin') format('svg');
-                font-weight: normal;
-                font-style: normal;
-            }
-            @font-face {
-                font-family: 'Swiss 721 W01 Light';
-                src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.eot');
-                src: url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.eot?#iefix') format('embedded-opentype'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.woff') format('woff'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.ttf') format('truetype'),
-                     url('https://s3.amazonaws.com/smartapp-icons/Partner/fonts/swiss-721-light-webfont.svg#swis721_lt_btlight') format('svg');
-                font-weight: normal;
-                font-style: normal;
-            }
-            .container {
-                width: 100%;
-                padding: 0px;
-                /*background: #eee;*/
-                text-align: center;
-            }
-            img {
-                vertical-align: middle;
-            }
-            img:nth-child(2) {
-                margin: 0 10px;
-            }
-            p {
-                font-size: 1.5em;
-                font-family: 'Swiss 721 W01 Thin';
-                text-align: center;
-                color: #666666;
-                padding: 0 40px;
-                margin-bottom: 0;
-            }
-        /*
-            p:last-child {
-                margin-top: 0px;
-            }
-        */
-            span {
-                font-family: 'Swiss 721 W01 Light';
-            }
-        </style>
-        </head>
-        <body>
-            <div class="container">
-                <img src=""" + getVendorIcon() + """ alt="Vendor icon" style="width: 30%;max-height: 30%"/>
-                <img src="https://s3.amazonaws.com/smartapp-icons/Partner/support/connected-device-icn%402x.png" alt="connected device icon" style="width: 10%;max-height: 10%"/>
-                <img src="https://s3.amazonaws.com/smartapp-icons/Partner/support/st-logo%402x.png" alt="SmartThings logo" style="width: 30%;max-height: 30%"/>
-                <p>Your Quirky account is now connected to SmartThings. Tap 'Done' to continue to choose devices.</p>
-			</div>
-        </body>
-        </html>
-        """
-	render contentType: 'text/html', data: html
-}
 
 String toQueryString(Map m) {
         return m.collect { k, v -> "${k}=${URLEncoder.encode(v.toString())}" }.sort().join("&")
+}
+
+
+def checkToken() {
+	log.debug "In checkToken"
+    
+    def tokenStatus = "bad"
+    
+    //check existing token by calling user device list
+    try {
+    	httpPost([ uri		: apiUrl(),
+    		  	  path 		: "controls/list",
+               	  headers 	: [ 'Authorization' : 'Bearer ' + state.vendorAccessToken ]
+		])
+		{ response ->
+        	log.debug "Response is: ${response.status}"
+            if ( response.status == 200 ) {
+            	log.debug "The current token is good"
+                tokenStatus = "good"
+            }
+		}
+	}
+    catch(Exception e) {
+    	log.debug "Current access token did not work. Trying refresh Token now."
+    }
+
+	if ( tokenStatus == "bad" ) {
+        //Let's try the refresh token now
+    	log.debug "Trying to refresh tokens"
+        
+    	def tokenParams = [ client_id		: getClientId(),
+        					client_secret	: getClientSecret(),
+							grant_type		: "refresh_token",
+							refresh_token	: state.vendorRefreshToken ]
+        
+    	try {
+			httpPost(getVendorTokenPath(),toQueryString(tokenParams) ) { response ->
+            	debugOut "Successfully refreshed tokens with code: ${response.status}"
+            	state.vendorRefreshToken = response.data.refresh_token
+            	state.vendorAccessToken = response.data.access_token
+            	tokenStatus = "good"
+        	}
+		}
+    	catch(Exception e) {
+    		log.debug "Unable to refresh token. Error ${e}"
+    	}
+	}
+    
+    if ( tokenStatus == "bad" ) {
+    	return "Error: Unable to refresh Token"
+    } else {
+    	return null //no errors
+    }
+}
+
+def apiPost(String path, Closure callback)
+{
+	log.debug "In apiPost with path: $path"
+    
+    //check to see if our token has expired
+    def status = checkToken()
+    log.debug "Status of checktoken: ${status}"
+    
+    if ( status ) {
+    	log.debug "Error! Status: ${status}"
+        return
+    } else {
+    	log.debug "Token is good. Call the command"
+    }
+    
+	httpPost([
+		uri : apiUrl(),
+		path : path,
+		headers : [ 'Authorization' : 'Bearer ' + state.vendorAccessToken ]
+	])
+	{
+		response ->
+			callback.call(response)
+	}
 }
