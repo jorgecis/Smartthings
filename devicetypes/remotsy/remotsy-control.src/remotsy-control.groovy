ae95c43919248f9ca84d7c372aeb8e729a2b34cb
Jorge Cisneros <jorgecis@gmail.com>
2018-03-04 21:24:56 -0600

    Save working code

--- devicetypes/remotsy/remotsy-control.src/remotsy-control.groovy
+++ devicetypes/remotsy/remotsy-control.src/remotsy-control.groovy
@@ -13,113 +13,202 @@
  */
 metadata {
 	definition (name: "Remotsy Control", namespace: "remotsy", author: "jorgecis@gmail.com") {
-		capability "Switch"
-        command "poweron"
-		command "poweroff"
-        command "mute"
+   		capability "switch" 
+            command "power_toggle"
+			command "mute" 
+			command "source"
+			command "menu"    
+            command "tools"           
+			command "HDMI"    
+            command "Sleep"
+            command "Up"
+            command "Down"
+            command "Left"
+            command "Right" 
+			command "chup" 
+ 			command "chdown"               
+			command "prech"
+			command "volup"    
+            command "voldown"           
+            command "Enter"
+            command "Return"
+            command "Exit"
 	}
 
-tiles (scale: 2){
-        standardTile("main", "device.status", canChangeIcon:true) {
-            state "default", label:'', icon:"st.Electronics.electronics15", backgroundColor:"#FFFFFF"
-    	}
-
- 		standardTile("titlemsg", "device.switch",  height:1, width:5, inactiveLabel:true, decoration:"flat") {
-            state "default", label:'Control Remote Samsung TV', icon:""
-        }
-
-		standardTile("blk", "device.status", inactiveLabel:true, decoration:"flat") {           
-        }
-        standardTile("poweron", "device.status", inactiveLabel:false, decoration:"flat") {
-            state "default", label:'On', action:"on"
-           
-        }
-
-        standardTile("poweroff", "device.status", inactiveLabel:false, decoration:"flat") {
-            state "default", label:'Off', action:"off"
-            
-        }
-        
-         standardTile("mute", "device.status", inactiveLabel:false, decoration:"flat") {
-            state "default", label:'mute',  action:"mute"
-            
-        }
-
-  
-
-       
-
-        main(["main"])
-
-        details([
-            "titlemsg","blk",
-            "poweron",
-            "poweroff",
-            "mute"
-            
-        ])
+    standardTile("switch", "device.status", width: 1, height: 1, canChangeIcon: true) {
+        state "default", label:'', icon:"https://s3.amazonaws.com/remotsy/logo256.png", backgroundColor:"#ffffff"
     }
-	
-}
-
-
-    
-def sendcmd(cmd) {
-
-	def params = [
-    	uri: "https://remotsy.com/control_t.php?cmd=${cmd}&id=dev1"
-    	
-    ]
-
-	try {
-    	httpGet(params) { resp ->
-    		if (resp.data) {
-        		log.debug "Response Data = ${resp.data}"
-        		log.debug "Response Status = ${resp.status}"
-
-                resp.headers.each {
-  					log.debug "header: ${it.name}: ${it.value}"
-				}
-        	}
-        	if(resp.status == 200) {
-	        	log.debug "Request was OK"
-    		}
-        	else {
-        		log.error "Request got http status ${resp.status}"
-        	}
-        }
-    } catch(e)
-    {
-    	log.debug e
+    standardTile("power", "device.switch", width: 1, height: 1, canChangeIcon: false) {
+        state "default", label:'Power', action:"power_toggle", decoration: "flat", icon:"st.samsung.da.RC_ic_power", backgroundColor:"#ffffff"
+    }   
+    standardTile("mute", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Mute', action:"mute", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff"
+    }    
+	standardTile("source", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Source', action:"source", icon:"st.Electronics.electronics15"
     }
+	standardTile("tools", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Tools', action:"tools", icon:"st.secondary.tools"
+    }
+	standardTile("menu", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Menu', action:"menu", icon:"st.vents.vent"
+    }
+	standardTile("HDMI", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Source', action:"HDMI", icon:"st.Electronics.electronics15"
+    }
+    standardTile("Sleep", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Sleep', action:"Sleep", icon:"st.Bedroom.bedroom10"
+    }
+    standardTile("Up", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Up', action:"Up", icon:"st.thermostat.thermostat-up"
+    }
+    standardTile("Down", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Down', action:"Down", icon:"st.thermostat.thermostat-down"
+    }
+    standardTile("Left", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Left', action:"Left", icon:"st.thermostat.thermostat-left"
+    }
+    standardTile("Right", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Right', action:"Right", icon:"st.thermostat.thermostat-right"
+    }  
+	standardTile("chup", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'CH Up', action:"chup", icon:"st.thermostat.thermostat-up"
+    }
+	standardTile("chdown", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'CH Down', action:"chdown", icon:"st.thermostat.thermostat-down"
+    }
+	standardTile("prech", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Pre CH', action:"prech", icon:"st.secondary.refresh-icon"
+    }
+    standardTile("volup", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Vol Up', action:"volup", icon:"st.thermostat.thermostat-up"
+    }
+    standardTile("voldown", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Vol Down', action:"voldown", icon:"st.thermostat.thermostat-down"
+    }
+    standardTile("Enter", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Enter', action:"Enter", icon:"st.illuminance.illuminance.dark"
+    }
+    standardTile("Return", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Return', action:"Return", icon:"st.secondary.refresh-icon"
+    }
+    standardTile("Exit", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:'Exit', action:"Exit", icon:"st.locks.lock.unlocked"
+    } 
+    standardTile("blank", "device.switch", decoration: "flat", canChangeIcon: false) {
+        state "default", label:''
+    } 
+      
+    main "switch"
+    details (["power","blank","Sleep","chup","prech","volup","chdown","mute","voldown", "menu", "Up", "tools", "Left", "Enter", "Right", "Return", "Down", "Exit"])	
 }
-
-
 
 def parse(String description) {
 	log.debug(description)
-}
-
-
-def on() {
-	log.debug "poweron"
-    sendcmd("poweron")
+    return null
 }
 
 def off() {
-	log.debug "poweroff"
-    sendcmd("poweroff")
+	log.debug "Power OFF"
+    parent.IRAction("POWER OFF",device.deviceNetworkId) 
 }
 
+def on() {
+	log.debug "Power ON"
+    parent.IRAction("POWER ON",device.deviceNetworkId) 
+}
+
+def power_toggle() {
+	log.debug "POWER TOGGLE"
+    parent.IRAction("POWER TOGGLE",device.deviceNetworkId) 
+}
 
 def mute() {
-	log.debug "mute"
-    sendcmd("mute")
+	log.trace "MUTE pressed"
+    parent.IRAction("MUTE TOGGLE",device.deviceNetworkId) 
 }
 
-
-
-def installed() {
-   log.debug "Installed with settings: ${settings}"
+def source() {
+	log.debug "SOURCE pressed"
+    parent.IRAction("SOURCE",device.deviceNetworkId) 
 }
 
+def menu() {
+	log.debug "MENU pressed"
+    parent.IRAction("MENU TOGGLE",device.deviceNetworkId) 
+}
+
+def tools() {
+	log.debug "TOOLS pressed"
+    parent.IRAction("TOOLS",device.deviceNetworkId) 
+}
+
+def HDMI() {
+	log.debug "HDMI pressed"
+    parent.IRAction("HDMI",device.deviceNetworkId) 
+}
+
+def Sleep() {
+	log.debug "SLEEP pressed"
+    parent.IRAction("SLEEP",device.deviceNetworkId) 
+}
+
+def Up() {
+	log.debug "UP pressed"
+    parent.IRAction("CURSOR UP",device.deviceNetworkId)
+}
+
+def Down() {
+	log.debug "DOWN pressed"
+    parent.IRAction("CURSOR DOWN",device.deviceNetworkId) 
+}
+
+def Left() {
+	log.debug "LEFT pressed"
+    parent.IRAction("CURSOR LEFT",device.deviceNetworkId) 
+}
+
+def Right() {
+	log.debug "RIGHT pressed"
+    parent.IRAction("CURSOR RIGHT",device.deviceNetworkId) 
+}
+
+def chup() {
+	log.debug "CHUP pressed"
+    parent.IRAction("CHANNEL UP",device.deviceNetworkId)
+}
+
+def chdown() {
+	log.debug "CHDOWN pressed"
+    parent.IRAction("CHANNEL DOWN",device.deviceNetworkId) 
+}
+
+def prech() {
+	log.debug "PRECH pressed"
+    parent.IRAction("PREVIOUS CHANNEL",device.deviceNetworkId)
+}
+
+def Exit() {
+	log.debug "EXIT pressed"
+    parent.IRAction("EXIT",device.deviceNetworkId) 
+}
+
+def volup() {
+	log.debug "VOLUP pressed"
+    parent.IRAction("VOLUME UP",device.deviceNetworkId)
+}
+
+def voldown() {
+	log.debug "VOLDOWN pressed"
+    parent.IRAction("VOLUME DOWN",device.deviceNetworkId) 
+}
+
+def Enter() {
+	log.debug "ENTER pressed"
+    parent.IRAction("CURSOR ENTER",device.deviceNetworkId) 
+}
+
+def Return() {
+	log.debug "RETURN pressed"
+    parent.IRAction("RETURN",device.deviceNetworkId) 
+}
